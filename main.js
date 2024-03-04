5210269d3aee6ede75c8b2f2b6f33121babc50cd
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-03-04 18:42:02 +0200

    Update weather display and search functionality

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 const apiKey = "0827b0d8fb95a27f3b4227fb339650f8";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=cairo";
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid="${apiKey}`);
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data)
+
+
+    // searchBox.addEventListener("click", function () {
+    //     var city = document.querySelector("#search").value;
+    //     checkWeather(city);
+    // });
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})   
