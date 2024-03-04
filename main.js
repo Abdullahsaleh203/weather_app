987679ad3cba948a89314795e02519eeff106e5e
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-03-04 19:34:41 +0200

    Add weather icon based on weather condition

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
@@ -18,7 +19,20 @@ async function checkWeather(city) {
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-}
+    if (data.weather[0].main == "Cloud") {
+        weatherIcon.src = "images/cloudy.png";
+    } if else (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    }
+    } if else (data.weather[0].main == "Clear") {
+    weatherIcon.src = "images/clear.png";
+    } if else (data.weather[0].main == "Clear") {
+    weatherIcon.src = "images/clear.png";
+    }
+    
+
+    // console.log(data)
+
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 })   
